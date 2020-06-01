335e58c93643d3c2f2599ba3447d42ae706809e5
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-06-01 00:53:41 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,10 @@ class Backer
     @backed_projects = []
   end
 
+  def add_project(project)
+    @back_projects << project
+  end
+
   def back_project(project)
     @backed_projects << project
     project.backers << self
