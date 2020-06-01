e3d3746cf4d7b812134f94253affcdec8a2c777d
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-06-01 00:46:25 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,10 @@
 class Project
 
-  attr_reader :title
+  attr_reader :title, :backed_projects
 
   def initialize(title)
     @title = title
+    @backed_projects = []
   end
 
   def initialize(backers)
